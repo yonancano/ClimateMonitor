2ab33fe8fec87ffecc4b25abbfbfd55482588864
ycanobbucket <yonan.cano@gmail.com>
2024-03-12 15:34:31 -0600

    fix: issue # 2 firmware validation

--- ClimateMonitor/Services/Models/DeviceReadingRequest.cs
+++ ClimateMonitor/Services/Models/DeviceReadingRequest.cs
@@ -5,6 +5,7 @@ namespace ClimateMonitor.Services.Models;
 public class DeviceReadingRequest
 {
     [Required]
+    [RegularExpression(pattern: @"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$", ErrorMessage = "The firmware value does not match semantic versioning format.")]
     public string FirmwareVersion { get; set; } = string.Empty;
 
     [Required]
